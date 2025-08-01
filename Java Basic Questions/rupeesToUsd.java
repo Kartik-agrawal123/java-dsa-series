01666d25d5e1352dc76d129054b53dcfc812a91f
kartik agrawal <kartik.agrawal@satincreditcare.com>
2025-08-01 16:10:18 +0530

    Input currency in rupees and output in USD

---
+++ Java Basic Questions/rupeesToUsd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class rupeesToUsd {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter value in INR");
+        int n = sc.nextInt();
+        float usd = (float) (n * 0.011);
+        System.out.println("Your USD amount is " + usd );
+    }
+}
