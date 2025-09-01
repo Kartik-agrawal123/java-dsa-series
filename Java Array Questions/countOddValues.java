54691872780a27b8b1e46f70ced897a1b8058635
kartik agrawal <kartik.agrawal@satincreditcare.com>
2025-09-01 12:04:08 +0530

    Leetcode 1252

--- Java Array Questions/countOddValues.java
+++ Java Array Questions/countOddValues.java
@@ -1,18 +1,28 @@
+import java.util.Arrays;
+
 public class countOddValues {
 
     public static void main(String[] args) {
         int m = 2;
-        int n= 3;
+        int n = 3;
+        int[] row = new int[m];
+        int[] col= new int [n];
         int[][] indices = {{0,1},{1,1}};
-        int[][] result = new int[m][n];
 
-        for(int i = 0;i<=indices.length-1;i++){
-            for(int j = 0;j<=indices[i].length-1;j++){
-                if(i == indices[i][j]){
-                    result[i][j]++;
+        for (int[] index : indices) {
+            row[index[0]]++;
+            col[index[1]]++;
+        }
+
+        int oddCount = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((row[i] + col[j]) % 2 == 1) {
+                    oddCount++;
                 }
             }
         }
+        System.out.print(oddCount);
     }
     
 }
